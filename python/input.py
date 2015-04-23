04fa7c749dda84f0ddf3ac3b717dd1a3dfce7ddf
Justin Bricker <jt.bricker@gmail.com>
2015-04-23 11:52:56 -0400

    created a python folder

---
+++ python/input.py
@@ -0,0 +1,12 @@
+
+def get_input():
+	print("Enter a string: "),
+	input_string = raw_input()
+	return input_string
+
+def main():
+	input_string = get_input()
+	print input_string
+
+if __name__ == '__main__':
+	main()
