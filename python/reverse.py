1269831a7b9672cf3e423377626a427fd9cd8d51
Justin Bricker <jt.bricker@gmail.com>
2015-04-23 12:50:46 -0400

    Now with palindrome checker!

--- python/reverse.py
+++ python/reverse.py
@@ -3,10 +3,14 @@ import input
 def reverse_string(in_str):
 	return in_str[::-1]
 
+def isPalindrome(in_str):
+	return in_str.lower() == in_str.lower()[::-1]
+
 def main():
 	input_string = input.get_input()
 	reversed_string = reverse_string(input_string)
 	print reversed_string
+	print "This string a Palindrome?: %s"%(isPalindrome(reversed_string))
 
 if __name__ == '__main__':
 	main()
